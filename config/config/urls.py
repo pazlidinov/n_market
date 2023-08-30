4d5913b38ec6d590fe18ef778eefb3838452204e
asliddin0012 <143064731+asliddin0012@users.noreply.github.com>
2023-08-30 14:05:50 +0500

    add

--- config/config/urls.py
+++ config/config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('product.urls', namespace='product')),
 ]
 if settings.DEBUG:
