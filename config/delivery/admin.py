68a0c73f08ac9700271c7ed90a71fdfcedbc7db5
asliddin0012 <143064731+asliddin0012@users.noreply.github.com>
2023-09-04 11:30:26 +0500

    add

--- config/delivery/admin.py
+++ config/delivery/admin.py
@@ -1,15 +1,15 @@
-# from django.contrib import admin
-# from .models import *
-# # Register your models here.
+from django.contrib import admin
+from .models import *
+# Register your models here.
 
 
 
-# @admin.register(DeliveredItem)
-# class DeliveredItem(admin.ModelAdmin):
-#     # list_display = ["id", 'slug', 'inn']
-#     prepopulated_fields = {"slug": ("name",)}
+@admin.register(DeliveredItem)
+class DeliveredItem(admin.ModelAdmin):
+    
+    prepopulated_fields = {"slug": ("name",)}
 
-# @admin.register(DeliveringCompany)
-# class DeliveringCompany(admin.ModelAdmin):
-#     # list_display = ["id", 'name', 'slug']
-#     prepopulated_fields = {"slug": ("company_name",)}
+@admin.register(DeliveringCompany)
+class DeliveringCompany(admin.ModelAdmin):
+  
+    prepopulated_fields = {"slug": ("company_name",)}
--- config/delivery/models.py
+++ config/delivery/models.py
@@ -1,35 +1,38 @@
-# from django.db import models
-# from product.models import Product
-# from django.contrib.auth.models import User
+from django.db import models
+from product.models import Product
+from django.contrib.auth.models import User
 
-# # Create your models here.
-
-
+# Create your models here.
 
 
-# class DeliveredItem(models.Model):
-#     company_name = models.ForeignKey(
-#         DeliverCompany, on_delete=models.CASCADE, related_name='delivering_company')
-#     name = models.CharField(max_length=150)
-#     slug = models.SlugField(max_length=150)
-#     delivery_name = models.CharField(max_length=150)
-#     weight = models.FloatField(blank=True, null=True)
-#     delivered_date = models.DateField(auto_now_add=True)
-#     region = models.CharField(max_length=150)
-#     product = models.ManyToManyField(Product, blank=True)
 
-#     def __str__(self):
-#         return self.name
-
-
-# class DeliverCompany(models.Model):
-#     company_name = models.CharField(max_length=150)
-#     slug = models.SlugField(max_length=150)
-#     inn = models.CharField(max_length=150)
-#     bank = models.CharField(max_length=150)
-#     code = models.IntegerField(blank=True, null=True)
-#     mfo = models.IntegerField(blank=True, null=True)
-#     xisob_raqam = models.IntegerField(blank=True, null=True)
+class DeliveringCompany(models.Model):
+    company_name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
+    inn = models.CharField(max_length=150)
+    bank = models.CharField(max_length=150)
+    code = models.IntegerField(blank=True, null=True)
+    mfo = models.IntegerField(blank=True, null=True)
+    xisob_raqam = models.IntegerField(blank=True, null=True)
     
-#     def __str__(self):
-#         return self.company_name
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
+        # ordering = ["-id"]
+        verbose_name_plural = "Delivering Companies"    
+
+class DeliveredItem(models.Model):
+    company_name = models.ForeignKey(
+        DeliveringCompany, on_delete=models.CASCADE, related_name='delivering_company', blank=True)
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
+    delivery_name = models.CharField(max_length=150)
+    weight = models.FloatField(blank=True, null=True)
+    delivered_date = models.DateField(auto_now_add=True)
+    region = models.CharField(max_length=150)
+    product = models.ManyToManyField(Product, blank=True)
+
+    def __str__(self):
+        return self.name
+    
