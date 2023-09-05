fa97fcfad673835cafbc9a83bcdb2c26b63c937a
iskandar006 <abdulatipovv2003@gmail.com>
2023-09-05 11:45:09 +0500

    add

--- config/accounts/views.py
+++ config/accounts/views.py
@@ -1,30 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import Group
 
 from django.contrib.auth.forms import UserCreationForm
-from django.views import generic
 
 
 def register(request):
     form = UserCreationForm()
-
     if request.method == "POST":
-
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            # add user to group
-            gp = Group.objects.get(name="users")
-            user.groups.add(gp.id)
-            user.save()
-            # login new user
-            login(request, user)
-            messages.add_message(request, messages.SUCCESS, "Well done !")
-            return redirect("/")
+            u = form.save()
+            try:
+                group = Group.objects.get(name="user")
+                group.user_set.add(u)
+            except Exception as er:
+                # raise
+                print(er)
+            authenticate(u)
+            return redirect("/accounts/login/")
         else:
-            messages.add_message(request, messages.WARNING,
-                                 "Something went wrong !")
-    else:
+            return render(request, "auth/register.html", {"form": form})
 
-        return render(request, "auth/register.html", {"form": form})
     return render(request, "auth/register.html", {"form": form})
+
+
