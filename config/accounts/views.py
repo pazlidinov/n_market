c52b63423cc200739c1a9ca102c9dceb0d22a542
iskandar006 <abdulatipovv2003@gmail.com>
2023-09-05 09:00:56 +0500

    add

--- config/accounts/views.py
+++ config/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.urls import reverse_lazy
+
 from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
 
