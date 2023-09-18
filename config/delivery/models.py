b6ea98d9eae7b28b0a62394fe0298d1d805e4541
asliddin0012 <143064731+asliddin0012@users.noreply.github.com>
2023-09-18 11:45:51 +0500

    add
    registratsiya yangilandi

--- config/delivery/models.py
+++ config/delivery/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from product.models import Product
-from accounts.models import User
+# from accounts.models import User
 # Create your models here.
 
 
@@ -31,7 +31,7 @@ class DeliveredItem(models.Model):
     extra_cost = models.IntegerField(default=0)
     delivered_date = models.DateTimeField(auto_now_add=True)
     region = models.CharField(max_length=150)
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    # recipient = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.company_name
