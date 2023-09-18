65c6a73f40ebfd44c498fba9f53edb14b06e7b7d
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-09-18 12:29:21 +0500

    aa

--- config/accounts/views.py
+++ config/accounts/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import Group
-
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.base import TemplateView
+from .forms import UserRegisterForm
+
+
+
 
 def register(request):
-    form = UserCreationForm()
+    form = UserRegisterForm()
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # try:
-            #     group = Group.objects.get(name="user")
-            #     group.user_set.add(u)
-            # except Exception as er:
-            #     authenticate(u)
+            try:
+                group = Group.objects.get(name="Default User")
+                group.user_set.add(user)
+            except Exception as er:
+                print(er)
             authenticate(user)
-            return redirect("/accounts/login/")
-        else:
-            return render(request, "auth/register.html", {"form": form})
-
+            login(request, user)
+        return redirect('/accounts/login')
     return render(request, "auth/register.html", {"form": form})
 
 
--- config/product/views.py
+++ config/product/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+from .models import Product
 # Create your views here.
 
 
@@ -7,11 +10,13 @@ class Indicators(TemplateView):
     template_name = 'indicators.html'
 
 
-class ProductsView(TemplateView):
+class ProductsView(ListView):
+    model = Product
     template_name = 'products.html'
-    
+
+
 class SaledProduct(TemplateView):
     template_name = 'sales.html'
-    
-    
-# class 
+
+
+# class
